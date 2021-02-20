b130d4afcf734d027bf5fe1167b52bc1f9ecf8e4
10117161RakaPanjiM <73197135+10117161RakaPanjiM@users.noreply.github.com>
2021-02-20 14:24:39 +0700

    Add files via upload

---
+++ UAS-10117161-RakaPanjiM-SoalNo4/src/uas/pkg10117161/rakapanjim/soalno4/UAS10117161RakaPanjiMSoalNo4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.pkg10117161.rakapanjim.soalno4;
+
+/**
+ *
+ * @author GPS-2
+ */
+public class UAS10117161RakaPanjiMSoalNo4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
